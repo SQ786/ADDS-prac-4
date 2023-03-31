4943a919200521f2796c10af34f1b600c43b6c03
Sakhawat <a1860466@adelaide.edu.au>
2023-03-31 14:08:28 +1030

    p

--- Finder.cpp
+++ Finder.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
     vector<int> Finder::findSubstrings(string s1, string s2) {
     vector<int> result;
-
+      int pos = 0;
+      
     for(size_t i = 1; i <= s2.size(); i++) {
-        int pos = 0;
+  
     size_t found = s1.find(s2.substr(0, i), pos);
     if (found != string::npos) {
         result.push_back(found);
